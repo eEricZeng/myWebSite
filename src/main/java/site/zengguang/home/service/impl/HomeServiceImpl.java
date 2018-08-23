2a0ef3b5d4560beda1af1a27a259ef4865e16872
ericzeng <549373335@qq.com>
2018-08-23 21:32:33 +0800

    chore: 取消打印调试信息

--- src/main/java/site/zengguang/home/service/impl/HomeServiceImpl.java
+++ src/main/java/site/zengguang/home/service/impl/HomeServiceImpl.java
@@ -57,7 +57,7 @@ public class HomeServiceImpl implements IHomeService{
         //1、创建session
         Session session = Session.getInstance(prop);
         //开启Session的debug模式，这样就可以查看到程序发送Email的运行状态
-        session.setDebug(true);
+        //session.setDebug(true);
         //2、通过session得到transport对象
         Transport ts = null;
         try {
