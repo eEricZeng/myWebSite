cd342a0bd80adcf74e196b731a24cd8045a2fc2f
ericzeng <549373335@qq.com>
2018-08-26 21:54:01 +0800

    chore: 增加共有方法，获取contextPath

---
+++ src/main/webapp/skins/js/common/mywebsite.js
@@ -0,0 +1,7 @@
+/*静态页面获取contenxPath*/
+function getContextPath() {
+    var pathName = document.location.pathname;
+    var index = pathName.substr(1).indexOf("/");
+    var result = pathName.substr(0, index + 1);
+    return result;
+}
